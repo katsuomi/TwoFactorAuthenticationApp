38c0824373324dfd4da753535a813a816c50cb1b
青木克臣 <mugichak1227@gmail.com>
2019-12-06 13:45:18 +0900

    update component

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -1,30 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 import firebase from './firebase/firebase'
 
-class GoogleAuth extends Component {
-  constructor(){
-    super()
-    this.SubmitGoogleAuth = this.SubmitGoogleAuth.bind(this);
+const GoogleAuth = () => {
+  const SubmitGoogleAuth = () => {
+    const provider =  new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
   }
 
-
-  async SubmitGoogleAuth(){
-    const provider = await new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithPopup(provider)
-  }
-
-  render() {
-    return (
-      <div style={{textAlign: "center" ,marginTop: "60px"}}>
-        <Button variant="contained" color="secondary" onClick={this.SubmitGoogleAuth} style={{width: "200px"}}>
-          GoogleでLogin
-        </Button>
-      </div>
-    )
-  }
+  return (
+    <div style={{textAlign: "center" ,marginTop: "60px"}}>
+      <Button variant="contained" color="secondary" onClick={SubmitGoogleAuth} style={{width: "200px"}}>
+        GoogleでLogin
+      </Button>
+    </div>
+  )
 }
-
-
 
 export default GoogleAuth
