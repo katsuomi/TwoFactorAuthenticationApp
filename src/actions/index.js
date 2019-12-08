c2bcea15c28da9750c04fef36f16e3feeba7ecc6
katsuomi <mugichak1227@gmail.com>
2019-12-08 17:26:36 +0900

    update action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,21 @@
-export const GOOGLE_AUTH = 'GOOGLE_AUTH'
-export const PHONE_AUTH = 'PHONE_AUTH'
-export const CURRENT_USER = 'CURRENT_USER'
+import firebase from '../firebase/firebase';
+
+export const GOOGLE_AUTH = 'GOOGLE_AUTH';
+export const PHONE_AUTH = 'PHONE_AUTH';
+
+export const googleAuth = () => {
+  console.log("+++++++++++++++++")
+  const provider =  new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+  .then((result) => {
+    console.log("success:",result)
+    const current_user = result.user
+    // dispatch({
+    //   type: GOOGLE_AUTH,
+    //   current_user
+    // })
+  })
+  .catch(err => {
+    alert(err)
+  })
+}
