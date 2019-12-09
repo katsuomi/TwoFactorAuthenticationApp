17b3ab2d1fd65450644b322b66f790c3c74f633d
青木克臣 <mugichak1227@gmail.com>
2019-12-09 13:00:09 +0900

    update action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,7 @@
 import firebase from '../firebase/firebase';
 
-export const GOOGLE_AUTH = 'GOOGLE_AUTH';
+export const AUTH = 'AUTH';
 export const PHONE_NUMBER_CONFIRM = 'PHONE_NUMBER_CONFIRM';
-export const PHONE_NUMBER_AUTH = 'PHONE_NUMBER_AUTH';
 
 export const googleAuth = (dispatch) => {
   const provider =  new firebase.auth.GoogleAuthProvider();
@@ -11,7 +10,7 @@ export const googleAuth = (dispatch) => {
     console.log("success:",result)
     const current_user = result.user
     dispatch({
-      type: GOOGLE_AUTH,
+      type: AUTH,
       current_user
     })
   })
@@ -20,13 +19,29 @@ export const googleAuth = (dispatch) => {
   })
 }
 
-export const phoneNumberConfirm = (phoneNumber,appVerifier,dispatch) => {
+export const phoneNumberConfirm = ( phoneNumber,appVerifier,dispatch ) => {
   firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
   .then((result) => {
     console.log("success:",result)
     dispatch({
       type: PHONE_NUMBER_CONFIRM,
       result
+    })
+  })
+  .catch(err => {
+    alert(err)
+  })
+}
+
+export const phoneNumberAuth = ( verificationId,secretNumber,dispatch ) => {
+  const credential = firebase.auth.PhoneAuthProvider.credential(verificationId, secretNumber);
+  firebase.auth().currentUser.linkAndRetrieveDataWithCredential(credential)
+  .then((result) => {
+    console.log("success:",result)
+    const current_user = result.user
+    dispatch({
+      type: AUTH,
+      current_user
     })
   })
   .catch(err => {
