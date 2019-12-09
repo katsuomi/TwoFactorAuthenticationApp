506fe3f5b8591615fb7cc69d57acf3deb7b5d8e7
青木克臣 <mugichak1227@gmail.com>
2019-12-09 13:01:09 +0900

    update reducer

--- src/reducers/auths.js
+++ src/reducers/auths.js
@@ -1,26 +1,17 @@
 import {
-  GOOGLE_AUTH,
-  PHONE_NUMBER_AUTH,
-  PHONE_NUMBER_CONFIRM,
-} from '../actions'
+  AUTH,
+} from '../actions';
 
 const initialState = { 
-  current_user: {},
-  phone_number_confirmation: {}
+  current_user: {}
 }
 
 const auths = (state = initialState, action) => {
   switch(action.type) {
-    case GOOGLE_AUTH:
+    case AUTH:
       return {
         current_user: action.current_user
       }
-    case PHONE_NUMBER_CONFIRM:
-      return {
-        phone_number_confirmation: action.result
-      }
-    case PHONE_NUMBER_AUTH:
-      return []
     default:
       return state
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import auths from './auths'
+import phoneNumberConfirms from './phoneNumberConfirms'
 
 export default combineReducers({
-  auths,
+  auths,phoneNumberConfirms
 })
