1390efde5b57514c374a799c6158b82daeaa57ac
katsuomi <mugichak1227@gmail.com>
2019-12-09 11:12:57 +0900

    update reducer

--- src/reducers/auths.js
+++ src/reducers/auths.js
@@ -1,13 +1,25 @@
 import {
   GOOGLE_AUTH,
-  PHONE_AUTH,
+  PHONE_NUMBER_AUTH,
+  PHONE_NUMBER_CONFIRM,
 } from '../actions'
 
-const auths = (state = [], action) => {
+const initialState = { 
+  current_user: {},
+  phone_number_confirmation: {}
+}
+
+const auths = (state = initialState, action) => {
   switch(action.type) {
     case GOOGLE_AUTH:
-      return []
-    case PHONE_AUTH:
+      return {
+        current_user: action.current_user
+      }
+    case PHONE_NUMBER_CONFIRM:
+      return {
+        phone_number_confirmation: action.result
+      }
+    case PHONE_NUMBER_AUTH:
       return []
     default:
       return state
