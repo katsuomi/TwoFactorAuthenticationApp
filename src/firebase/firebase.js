bf5a33a0bc07d9937f1a221326e00bd387c924fc
青木克臣 <mugichak1227@gmail.com>
2019-12-09 13:00:50 +0900

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCutWc4_Ur_oPdiGo8mLW_q9l3xUL7CZ4s",
-  authDomain: "two-factor-auth-app.firebaseapp.com",
-  databaseURL: "https://two-factor-auth-app.firebaseio.com",
-  projectId: "two-factor-auth-app",
-  storageBucket: "two-factor-auth-app.appspot.com",
-  messagingSenderId: "932198160011",
-  appId: "1:932198160011:web:40a56c84127066477b668e",
-  measurementId: "G-3PQ9SMTLSD"
+  apiKey: "***********",
+  authDomain: "***********",
+  databaseURL: "***********",
+  projectId: "***********",
+  storageBucket: "***********",
+  messagingSenderId: "***********",
+  appId: "***********",
+  measurementId: "***********",
 }
 
 firebase.initializeApp(config);
