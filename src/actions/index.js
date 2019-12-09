adeb2c2921bb45298fe680e8f31a36a655b907f4
katsuomi <mugichak1227@gmail.com>
2019-12-09 11:12:31 +0900

    update action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,33 @@
 import firebase from '../firebase/firebase';
 
 export const GOOGLE_AUTH = 'GOOGLE_AUTH';
-export const PHONE_AUTH = 'PHONE_AUTH';
+export const PHONE_NUMBER_CONFIRM = 'PHONE_NUMBER_CONFIRM';
+export const PHONE_NUMBER_AUTH = 'PHONE_NUMBER_AUTH';
 
-export const googleAuth = () => {
-  console.log("+++++++++++++++++")
+export const googleAuth = (dispatch) => {
   const provider =  new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider)
   .then((result) => {
     console.log("success:",result)
     const current_user = result.user
-    // dispatch({
-    //   type: GOOGLE_AUTH,
-    //   current_user
-    // })
+    dispatch({
+      type: GOOGLE_AUTH,
+      current_user
+    })
+  })
+  .catch(err => {
+    alert(err)
+  })
+}
+
+export const phoneNumberConfirm = (phoneNumber,appVerifier,dispatch) => {
+  firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+  .then((result) => {
+    console.log("success:",result)
+    dispatch({
+      type: PHONE_NUMBER_CONFIRM,
+      result
+    })
   })
   .catch(err => {
     alert(err)
